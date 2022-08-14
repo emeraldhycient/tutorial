5e444145c9953b2b2c8c9e7d6b2dc69e7bddfbb6
emeraldhycient <igwezehycient86@gmail.com>
2022-08-14 15:27:28 +0100

    [update] made new added usefect

--- src/components/home/Section2.jsx
+++ src/components/home/Section2.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./section2.css";
 import img1 from "../../assets/Ellipse 1.png";
 import img2 from "../../assets/Ellipse 2.png";
@@ -25,7 +25,22 @@ function Section2() {
 
   const [Popular, setPopular] = React.useState("");
 
-  React.useEffect(() => {
+  /*React.useEffect(() => {
+    setTimeout(() => {
+      setPopular([
+        { id: 1, name: "tomatoes", image: img1 },
+        { id: 2, name: "carrot", image: img2 },
+        { id: 3, name: "Cumcuber", image: img3 },
+        { id: 4, name: "Yellow Pepper", image: img4 },
+        { id: 5, name: "orange", image: img5 },
+        { id: 6, name: "apples", image: img6 },
+        { id: 7, name: "grapes", image: img8 },
+        { id: 8, name: "strawberry and apples", image: img9 },
+      ]);
+    }, 3000);
+  }, []);*/
+
+  useEffect(function () {
     setTimeout(() => {
       setPopular([
         { id: 1, name: "tomatoes", image: img1 },
@@ -45,38 +60,18 @@ function Section2() {
       <section>
         <h2 id="head">Popular Items</h2>
         <div className="imageholder">
-          <div className="hold">
-            <img src={img1} alt="" className="img" />
-            <h1>tomatoes</h1>
-          </div>
-          <div className="hold">
-            <img src={img2} alt="" className="img" />
-            <h1>carrot</h1>
-          </div>
-          <div className="hold">
-            <img src={img3} alt="" className="img" />
-            <h1>Cumcuber </h1>
-          </div>
-          <div className="hold">
-            <img src={img4} alt="" className="img" />
-            <h1>Yellow Pepper</h1>
-          </div>
-          <div className="hold">
-            <img src={img5} alt="" className="img" />
-            <h1>Oranges</h1>
-          </div>
-          <div className="hold">
-            <img src={img6} alt="" className="img" />
-            <h1>Apple</h1>
-          </div>
-          <div className="hold">
-            <img src={img8} alt="" className="img" />
-            <h1>Grapes</h1>
-          </div>
-          <div className="hold">
-            <img src={img9} alt="" className="img" />
-            <h1>Strewberry</h1>
-          </div>
+          {Popular ? (
+            Popular.map(function (item, index) {
+              return (
+                <div className="hold" key={item.id}>
+                  <img src={item.image} alt="" className="img" />
+                  <h1>{item.name}</h1>
+                </div>
+              );
+            })
+          ) : (
+            <div>loading...</div>
+          )}
         </div>
       </section>
       <section>
