31b1cd3f9472563cd62b52402e557645cc2221f6
emeraldhycient <igwezehycient86@gmail.com>
2022-08-28 15:23:15 +0100

    [udapte] added sticky top , changed the navbar to bootstrap mamanged

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,39 +2,35 @@ import { useState } from "react";
 import styles from "./header.module.css";
 import logo from "../../assets/logo.png";
 import { getStoredValue } from "../../utils/storgeHelper";
-import { Badge } from "react-bootstrap";
+import { Badge, Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function Header() {
   const [carts, setcarts] = useState(JSON.parse(getStoredValue("cart")) || []);
 
   return (
-    <nav className={styles.header}>
-      <div className={styles.logo}>
-        <img src={logo} />
-      </div>
-      <ul className={styles.list}>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/services">Services</a>
-        </li>
-        <li>
-          <a href="/shops">Shops</a>
-        </li>
-        <li>
-          <a href="/carts">
-            Carts <Badge bg="warning">{carts.length} </Badge>
-          </a>
-        </li>
-      </ul>
-      <button className={styles.button}>
-        <a href="/shop">Shop Now</a>
-      </button>
-    </nav>
+    <>
+      <Navbar bg="info" variant="dark" sticky="top">
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={logo} />
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/services">Services</Nav.Link>
+            <Nav.Link href="/shops">Shops</Nav.Link>
+            <Nav.Link href="/carts">
+              Carts <Badge bg="warning">{carts.length} </Badge>
+            </Nav.Link>
+            <Nav>
+              <Button variant="sucess">Login</Button>
+            </Nav>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
